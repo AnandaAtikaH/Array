836a81f086f9fced0f6dc122ad7feba3ef34eb94
AnandaAtikaH <nathenathe4@gmail.com>
2019-01-07 07:54:22 +0700

    first commit

---
+++ build/classes/array/Array.java
@@ -0,0 +1,18 @@
+
+package array;
+
+
+public class Array {
+
+
+    
+    public static void main(String[] args) {
+        String nama[] ={"ananda atika","nurul","diah","nisa","ramdan","siri","hira","wayuna","kayla","jassi"};
+             for (int a=0; a<11; a++){
+             System.out.println(nama[a]);
+            
+            }
+         }  
+    }
+    
+
